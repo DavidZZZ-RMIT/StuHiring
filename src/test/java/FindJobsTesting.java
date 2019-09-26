b1d1590e0a7c1336db7133c60f64daf8f2eff79c
Yu Jin <646952438@qq.com>
2019-09-26 13:24:07 +1000

    JUnit for test of finding jobs
    by inputting keywords about the job after logging in .

--- src/test/java/FindJobsTesting.java
+++ src/test/java/FindJobsTesting.java
@@ -21,7 +21,7 @@ import rmit.sef.assignment1.core.Student;
 
 public class FindJobsTesting {
 
-	@Before
+	//@Before
 	public void setUp() throws Exception {
 		StudentCollection stuContrl = new StudentCollection();
 		EmployerCollection empContrl = new EmployerCollection();
@@ -32,11 +32,53 @@ public class FindJobsTesting {
 
 		stuContrl.addStudent(new Student(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
 				faker.name().firstName(), faker.name().lastName(), faker.educator().university(),
+				"education", (random.nextBoolean() ? Gender.Female : Gender.Male),
+				DemoDataImporter.getRandomNumberInRange(1990, 2022),
+				new Availabilities(random.nextBoolean(), random.nextBoolean(), random.nextBoolean()),
+				ApplicantStatus.Available));
+		
+		
+		stuContrl.addStudent(new Student(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
+				faker.name().firstName(), faker.name().lastName(), faker.educator().university(),
 				faker.educator().course(), (random.nextBoolean() ? Gender.Female : Gender.Male),
 				DemoDataImporter.getRandomNumberInRange(1990, 2022),
 				new Availabilities(random.nextBoolean(), random.nextBoolean(), random.nextBoolean()),
 				ApplicantStatus.Available));
+		
+		stuContrl.addStudent(new Student(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
+				faker.name().firstName(), faker.name().lastName(), faker.educator().university(),
+				faker.educator().course(), (random.nextBoolean() ? Gender.Female : Gender.Male),
+				DemoDataImporter.getRandomNumberInRange(1990, 2022),
+				new Availabilities(random.nextBoolean(), random.nextBoolean(), random.nextBoolean()),
+				ApplicantStatus.Available));
+		
+		stuContrl.addStudent(new Student(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
+				faker.name().firstName(), faker.name().lastName(), faker.educator().university(),
+				faker.educator().course(), (random.nextBoolean() ? Gender.Female : Gender.Male),
+				DemoDataImporter.getRandomNumberInRange(1990, 2022),
+				new Availabilities(random.nextBoolean(), random.nextBoolean(), random.nextBoolean()),
+				ApplicantStatus.Available));
+		
 
+		empContrl.addEmployer(new Employer(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
+				faker.name().firstName(), faker.name().lastName(), "Thai style","Massage"));
+		empContrl.addEmployer(new Employer(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
+				faker.name().firstName(), faker.name().lastName(), "Smile Thai","Restaurant"));
+		empContrl.addEmployer(new Employer(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
+				faker.name().firstName(), faker.name().lastName(), faker.company().name(),
+				faker.company().profession()));
+		empContrl.addEmployer(new Employer(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
+				faker.name().firstName(), faker.name().lastName(), faker.company().name(),
+				"Chinese Education"));
+		empContrl.addEmployer(new Employer(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
+				faker.name().firstName(), faker.name().lastName(), faker.company().name(),
+				"Russion Education"));
+		empContrl.addEmployer(new Employer(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
+				faker.name().firstName(), faker.name().lastName(), faker.company().name(),
+				"Spanish Education"));
+		empContrl.addEmployer(new Employer(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
+				faker.name().firstName(), faker.name().lastName(), faker.company().name(),
+				faker.company().profession()));
 		empContrl.addEmployer(new Employer(faker.name().username(), "123456", faker.name().username() + "@gmail.com",
 				faker.name().firstName(), faker.name().lastName(), faker.company().name(),
 				faker.company().profession()));
@@ -45,36 +87,82 @@ public class FindJobsTesting {
 				DemoDataImporter.getRandomDate(DemoDataImporter.getRandomNumberInRange(0, 30)),
 				DemoDataImporter.getRandomDate(DemoDataImporter.getRandomNumberInRange(30, 90)),
 				new Availabilities(random.nextBoolean(), random.nextBoolean(), random.nextBoolean())));
+		jobContrl.add(new Job("", faker.job().title(), faker.job().position(),
+				DemoDataImporter.getRandomDate(DemoDataImporter.getRandomNumberInRange(0, 30)),
+				DemoDataImporter.getRandomDate(DemoDataImporter.getRandomNumberInRange(30, 90)),
+				new Availabilities(random.nextBoolean(), random.nextBoolean(), random.nextBoolean())));
+		jobContrl.add(new Job("", faker.job().title(), faker.job().position(),
+				DemoDataImporter.getRandomDate(DemoDataImporter.getRandomNumberInRange(0, 30)),
+				DemoDataImporter.getRandomDate(DemoDataImporter.getRandomNumberInRange(30, 90)),
+				new Availabilities(random.nextBoolean(), random.nextBoolean(), random.nextBoolean())));
+		jobContrl.add(new Job("", faker.job().title(), faker.job().position(),
+				DemoDataImporter.getRandomDate(DemoDataImporter.getRandomNumberInRange(0, 30)),
+				DemoDataImporter.getRandomDate(DemoDataImporter.getRandomNumberInRange(30, 90)),
+				new Availabilities(random.nextBoolean(), random.nextBoolean(), random.nextBoolean())));
+		jobContrl.add(new Job("", faker.job().title(), faker.job().position(),
+				DemoDataImporter.getRandomDate(DemoDataImporter.getRandomNumberInRange(0, 30)),
+				DemoDataImporter.getRandomDate(DemoDataImporter.getRandomNumberInRange(30, 90)),
+				new Availabilities(random.nextBoolean(), random.nextBoolean(), random.nextBoolean())));
+	}
+
+	@Test
+	public void LoginAsStudent() {
+		JSONObject json = new JSONObject();
+		json.put("email", "xxx");
+		json.put("pwd", "xxx");
+
+		JSONObject result = WebTestHelper.request("http://localhost:4567/loginAsStudent", json);
+
+		assertTrue(result.has("result") || ((String) result.get("result")).equals("succss"));
+
 	}
 
 	@Test
 	public void findJobsWithoutLogin() {
 		JSONObject json = new JSONObject();
-
-		json.put("keywords", "Java developer");
+		String keywords = "Education";
+		json.put("keywords", "Education");
 		JSONObject result = WebTestHelper.request("http://localhost:4567/findJobs", json);
 
 		Iterator<String> it = result.keys();
 		while (it.hasNext()) {
 			String title = it.next();
-			assertTrue(title.contains("Java developer")
-					|| ((String) ((JSONObject) result.get(title)).get("description")).contains("Java developer"));
+			assertTrue(title.contains(keywords)
+					|| ((String) ((JSONObject) result.get(title)).get("description")).contains(keywords));
 		}
 	}
 
 	@Test
-	public void findJobsAfterLoginAsStudent() {
-		fail("Not yet implemented");
+	public void findJobsAccordingToPreference() {
+		JSONObject json = new JSONObject();
+		String keywords = "Thai";
+		json.put("keywords", "Thai");
+		JSONObject result = WebTestHelper.request("http://localhost:4567/findJobs", json);
+		Iterator<String> it=result.keys();
+		while (it.hasNext()) {
+			String title = it.next();
+			assertTrue(title.contains(keywords)
+					|| ((String) ((JSONObject) result.get(title)).get("description")).contains(keywords));
+		}
 	}
 
 	@Test
-	public void findSpecialJobsAfterLoginAsStudent() {
-		fail("Not yet implemented");
-	}
+	public void findAllOfAvailableJobsAfterLogIn() {
+		JSONObject json = new JSONObject();
+		json.put("email", "xxx");
+		json.put("pwd", "xxx");
 
-	@Test
-	public void findJobsAfterLoginAsEmployer() {
-		fail("Not yet implemented");
+		JSONObject result = WebTestHelper.request("http://localhost:4567/loginAsStudent", json);
+
+		assertTrue(result.has("result") || ((String) result.get("result")).equals("succss"));
+		String keywords=" ";
+		json.put("keywords", " ");
+		Iterator<String> it=result.keys();
+		while (it.hasNext()) {
+			String title = it.next();
+			assertTrue(title.contains(keywords)
+					|| ((String) ((JSONObject) result.get(title)).get("description")).contains(keywords));
+		}
 	}
 
 }
